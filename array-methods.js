1492d85a07aad4a1d5c2fc1c9850f6b7190ee1c9
Katarina Dabo <katarina@usabilla.com>
2017-01-14 20:37:37 +0100

    array methods

--- array-methods.js
+++ array-methods.js
@@ -34,5 +34,93 @@ data.total = function(){
 };
 
 total = data.total();
-
 console.log(total);
+
+//concat
+var myArray = ['a', 'b', 'c'];
+var mySecondArray = [1, 2, 3];
+
+var sum = myArray.concat(mySecondArray);
+console.log(sum);
+
+var array2 = ['br', 'hr'],
+	array4 = ['de', 'hr', 'fr'],
+	array6 = ['ht', 'yu'];
+
+var sum2 = array2.concat(array4, array6);
+console.log(sum2)
+
+//join
+var whatever = ['snow', 'rain', 'sun'];
+var newWhatever = whatever.join();
+var newWhatever2 = whatever.join(' + ');
+console.log(newWhatever);
+console.log(newWhatever2);
+
+//pop
+var myFish = ['angel', 'clown', 'mandarin', 'sturgeon'];
+var popped = myFish.pop();
+console.log(myFish)
+
+//push
+var sports = ['soccer', 'baseball'];
+var total = ['swimming','diving'];
+
+Array.prototype.push.apply(total, sports);
+var total = sports.push('swimming','diving');
+console.log(total)
+
+//apply
+var vegetables = ['parsnip', 'potato'];
+var moreVegs = ['celery', 'beetroot'];
+
+Array.prototype.push.apply(vegetables, moreVegs);
+console.log(vegetables);
+console.log(moreVegs);
+
+//slice
+var myHonda = {
+	color: 'red',
+	wheels: 4,
+	engine: {
+		cylinders: 4,
+		size: 2.2
+	}
+};
+
+var myCar = [myHonda, 2, 'cherry condition', 'purchased 1997'];
+var newCar = myCar.slice(0, 2);
+console.log(newCar);
+
+//splice
+var myFish = ['angel', 'clown', 'mandarin', 'surgeon'];
+var removed = myFish.splice(0, 2, 'parrot', 'anemone', 'blue');
+console.log(removed);
+
+//sort, this method sort numbers, but doesn't sort things
+var fruit = ['cherries', 'apples', 'bananas'];
+fruit.sort();
+
+var scores = [1, 10, 2, 21];
+scores.sort();
+
+var things = ['word', 'Word', '1 Word', '2 Words'];
+things.sort();
+
+
+//if we want to sort array of simple values, create comparison function
+
+var m = ['aa', 'bb', 'a', 'b', 4, 8, 15, 16, 23, 42];
+m.sort(function (a, b) {
+    if (a === b) {
+        return 0;
+    }
+    if (typeof a === typeof b) {
+        return a < b ? -1 : 1;
+    }
+    return typeof a < typeof b ? -1 : 1;
+});
+
+console.log(m);
+
+
